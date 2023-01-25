58ea444c5a04c51bce932e4e1448056e0245dbd3
Francesco <francesco.mazzini432@gmail.com>
2023-01-25 10:09:58 +0100

    Back to the original function; Backend says that the map is not recognized as function maybe server version javascript is too old
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- controllers/stickers.controllers.ts
+++ controllers/stickers.controllers.ts
@@ -41,123 +41,100 @@ export const createStickerPack = (connection: Database) : any => {
                         const ID = rows[0].ID;
                         
                         let error : undefined | string = undefined;
+                                          
+                        if(tags)
+                            for(let i : number = 0; i < tags.length; i++) {
+        
+                                connection.query(
+                                    `INSERT INTO Tags (ID, tag)
+                                    VALUES (${ID}, '${tags[i]}');`,
+                                    function (err: any, rows: any, fields: any) {
+                                        if (err) 
+                                            if(error)
+                                                error += err;
+                                            else 
+                                                error = err;
                         
-                        const upload = async () : Promise<void> => {
-
-                            if(tags) {
-                                tags.map( (tag : string, i : number) =>  (
-                                    async () => connection.query(
-                                        `INSERT INTO Tags (ID, tag)
-                                        VALUES (${ID}, '${tag[i]}');`,
-                                        function (err: any, rows: any, fields: any) {
-                                            if (err) 
-                                                if(error)
-                                                    error += err;
-                                                else 
-                                                    error = err;
-                            
                                     })
-                                )).forEach(async (tagFunction : any) => {
-                                    await tagFunction();
-                                })
+        
                             }
+                        
+                        for(let i = 0; i < images.length; i++) {
+                            const image = images[i];
+        
+                            const uploadPath = "./public/files/temp/" + image.name;
+                            const outputPath = "./public/files/output/" + image.name.split(".")[0] + ".webp";
+                            image.mv(uploadPath, function(err: any) {
+                                if (err) {
+                                    console.log("Error");
+                                    fs.unlinkSync(uploadPath);
+                                } else {
 
-                            images.map( (imageElement : any, i : number) => (
-                                async () => {
+                                    try {
 
-                                    const image = imageElement[i];
-            
-                                    const uploadPath = "./public/files/temp/" + image.name;
-                                    const outputPath = "./public/files/output/" + image.name.split(".")[0] + ".webp";
-                                    image.mv(uploadPath, function(err: any) {
-                                        if (err) {
-                                            console.log("Error");
-                                            fs.unlinkSync(uploadPath);
-                                        } else {
+                                        sharp(uploadPath)
+                                        .webp({ quality: 30 })
+                                        .toFile(outputPath)
+                                        .then((res: any) => {
 
                                             try {
 
-                                                sharp(uploadPath)
-                                                .webp({ quality: 30 })
-                                                .toFile(outputPath)
-                                                .then((res: any) => {
-
-                                                    try {
-
-                                                        cloudinary.uploader
-                                                        .upload(outputPath)
-                                                        .then((info: any) => {
-                                                            const url = info.secure_url;
-                                
-                                                            fs.unlinkSync(uploadPath);
-                                                            fs.unlinkSync(outputPath);
-                                            
-                                                            connection.query(
-                                                                `INSERT INTO Image (ID, ordinal_order, image_file)
-                                                                VALUES (${ID}, ${i}, '${url}');`,
-                                                                function (err: any, rows: any, fields: any) {
-                                                                    if (err) 
-                                                                        if(error)
-                                                                            error += err;
-                                                                        else 
-                                                                            error = err;
-                                                                    
-                                                                })
+                                                cloudinary.uploader
+                                                .upload(outputPath)
+                                                .then((info: any) => {
+                                                    const url = info.secure_url;
+                        
+                                                    fs.unlinkSync(uploadPath);
+                                                    fs.unlinkSync(outputPath);
+                                    
+                                                    connection.query(
+                                                        `INSERT INTO Image (ID, ordinal_order, image_file)
+                                                        VALUES (${ID}, ${i}, '${url}');`,
+                                                        function (err: any, rows: any, fields: any) {
+                                                            if (err) 
+                                                                if(error)
+                                                                    error += err;
+                                                                else 
+                                                                    error = err;
+                                                            
                                                         })
-                                                        .catch((error: any) => {
-                                                            fs.unlinkSync(uploadPath);
-                                                            fs.unlinkSync(outputPath);
-                                                            console.log(error)
-                                                        });
-
-                                                    } catch (err : any) {
-
-                                                        console.log("Error");
-                                                        fs.unlinkSync(uploadPath);
-                                                        fs.unlinkSync(outputPath);
-
-                                                    }                                            
-
+                                                })
+                                                .catch((error: any) => {
+                                                    fs.unlinkSync(uploadPath);
+                                                    fs.unlinkSync(outputPath);
+                                                    console.log(error)
                                                 });
 
                                             } catch (err : any) {
+
                                                 console.log("Error");
                                                 fs.unlinkSync(uploadPath);
                                                 fs.unlinkSync(outputPath);
-                                            }
-                                            
 
-                                        }
-                
-                                    });
+                                            }                                            
 
-                                }
-                            )).forEach(async (imageUploadFunction : any) => {
-                                await imageUploadFunction();
-                            })
-                            
-                            // for(let i = 0; i < images.length; i++) {
+                                        });
 
-                            //     queryPromisesUpload.push(async () : Promise<void> => {
-
+                                    } catch (err : any) {
+                                        console.log("Error");
+                                        fs.unlinkSync(uploadPath);
+                                        fs.unlinkSync(outputPath);
+                                    }
                                     
 
-                            //     });
-
-                        
-                            // };
-
-
-                        };
-
-
-                        upload().then(() => {
-
-                            res.send({
-                                ID: ID
+                                }
+        
                             });
-
-                        })
+        
+                        
+                    
+                        }
+        
+                    
+                        res.send({
+                            ID: ID
+                        });
+        
         
                 });
 
